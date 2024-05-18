82934ffb0bd452fa1ce60cfcaa10e112903d1c12
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-05-18 10:47:24 +0800

    feat: transfer data between handlers

---
+++ 2-routing/1-middleware/3-transfer-data-between-handlers/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kataras/iris/v12"
+)
+
+const (
+	key   = "key"
+	value = "Hello world!"
+)
+
+func main() {
+	app := iris.New()
+
+	app.Get("/", myMiddleware, myHandler)
+
+	app.Listen(":8080")
+}
+
+func myMiddleware(ctx iris.Context) {
+	ctx.Values().Set(key, value)
+
+	ctx.Next() // must be used inside a middleware
+}
+
+func myHandler(ctx iris.Context) {
+	value := ctx.Values().GetString(key)
+	ctx.HTML(fmt.Sprintf("<h1>%s</h1>", value))
+}
